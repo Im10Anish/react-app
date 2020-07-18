2c7a4b8d09ff696cf50a4806a7d368c5ac0682aa
Anish Kumar <im10anish@gmail.com>
2020-07-18 14:40:36 +0530

    refactor: update rules for commitlint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
 	rules: {
+		'body-leading-blank': [1, 'always'],
+		'footer-leading-blank': [1, 'always'],
+		'header-max-length': [2, 'always', 72],
 		'type-enum': [2, 'always', ['major', 'minor', 'patch', 'refactor']],
 	},
 }
